c2b6e3c75f3a6ba2071fa97f69c8a6499b17c814
Utsab Maitra <utsab2366@gmail.com>
2026-05-01 14:45:24 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'personal-planner-v31'; // ভার্সন বাড়িয়ে দিন
+const CACHE_NAME = 'personal-planner-v32'; // ভার্সন বাড়িয়ে দিন
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
